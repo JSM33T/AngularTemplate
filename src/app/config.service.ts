be82d1c150f2b285188bc4d7592c83285000f9b0
jasmeet singh <jskainthofficial@gmail.com>
2023-11-25 09:28:42 +0530

    :rocket: updated api url

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -4,5 +4,5 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigService {
-  baseUrl = "https://almondcove.in/"
+  baseUrl = "https://site/almondcove.in/"
 }
